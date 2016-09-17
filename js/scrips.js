41143ec4cb8bfc9d301443ac6a89d344d5b2e0ec
Pattarapon <botpower7@gmail.com>
2016-09-18 03:05:08 +0700

    18/9/59

--- js/scrips.js
+++ js/scrips.js
@@ -33,6 +33,27 @@ $(document).ready(function() {
     $(window).resize(checkWidth);
 });
 
+$(document).ready(function() {
+    $(window).stellar();
+
+});
+
+$(document).ready(
+
+    function() {
+
+        $("html").niceScroll({
+            cursorcolor: "rgba(30,30,30,.5)",
+            zindex: 999,
+            scrollspeed: 100,
+            mousescrollstep: 50,
+            cursorborder: "0px solid #fff",
+        });
+
+
+    }
+
+);
 
 (function($) {
     "use strict"; // Start of use strict
@@ -65,27 +86,3 @@ $(document).ready(function() {
     })
 
 })(jQuery); // End of use strict
-
-
-
-$(function() {
-    $(".rslides").responsiveSlides();
-});
-$(".rslides").responsiveSlides({
-    auto: true, // Boolean: Animate automatically, true or false
-    speed: 500, // Integer: Speed of the transition, in milliseconds
-    timeout: 4000, // Integer: Time between slide transitions, in milliseconds
-    pager: false, // Boolean: Show pager, true or false
-    nav: false, // Boolean: Show navigation, true or false
-    random: false, // Boolean: Randomize the order of the slides, true or false
-    pause: false, // Boolean: Pause on hover, true or false
-    pauseControls: true, // Boolean: Pause when hovering controls, true or false
-    prevText: "Previous", // String: Text for the "previous" button
-    nextText: "Next", // String: Text for the "next" button
-    maxwidth: "", // Integer: Max-width of the slideshow, in pixels
-    navContainer: "", // Selector: Where controls should be appended to, default is after the 'ul'
-    manualControls: "", // Selector: Declare custom pager navigation
-    namespace: "rslides", // String: Change the default namespace used
-    before: function() {}, // Function: Before callback
-    after: function() {} // Function: After callback
-});
